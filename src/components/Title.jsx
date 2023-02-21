8a85437835f555b6f0bfb951f5d908f68272fadf
TonnyJP <tonny.piper@gmx.de>
2023-02-21 14:54:46 +0100

    working on the structure

---
+++ src/components/Title.jsx
@@ -0,0 +1,8 @@
+const Titel = ({title, subtitel}) => {
+    return(
+        <div className="section-title">
+            <h2>{title}<span>{subtitel}</span></h2>
+        </div>)
+}
+
+export default Titel;
---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import About from "./About";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Services from "./Services";
+import Tours from "./Tours";
+import Titel from "./Title";
+
+export {
+    About,
+    Footer,
+    Hero,
+    Navbar,
+    Services,
+    Tours,
+    Titel
+}
